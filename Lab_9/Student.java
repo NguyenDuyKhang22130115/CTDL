a014d0ecb87616bf84c0a6119b0f3383959b4b45
NguyenDuyKhang22130115 <146448483+NguyenDuyKhang22130115@users.noreply.github.com>
2023-12-12 13:04:04 +0700

    Add files via upload

---
+++ Lab_9/Student.java
@@ -0,0 +1,44 @@
+package Lab_9;
+
+public class Student {
+	private String id;
+	private String name;
+	private int year;
+
+	public Student(String id, String name, int year) {
+		super();
+		this.id = id;
+		this.name = name;
+		this.year = year;
+	}
+
+	public String getId() {
+		return id;
+	}
+
+	public void setId(String id) {
+		this.id = id;
+	}
+
+	public String getName() {
+		return name;
+	}
+
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	public int getYear() {
+		return year;
+	}
+
+	public void setYear(int year) {
+		this.year = year;
+	}
+
+	public int getYear1() {
+		// TODO Auto-generated method stub
+		return 0;
+	}
+
+}
